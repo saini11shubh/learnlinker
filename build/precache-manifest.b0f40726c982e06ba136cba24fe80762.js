a505b9b4002a1775da019f4924daed54301363ec
saini11shubh <107746129+saini11shubh@users.noreply.github.com>
2023-10-08 18:37:15 +0530

    update code

---
+++ build/precache-manifest.b0f40726c982e06ba136cba24fe80762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b6f5a279f66d3199b59949eb95bd75",
+    "url": "/learnlinker.github.io/index.html"
+  },
+  {
+    "revision": "136ff3353936c13c9cd8",
+    "url": "/learnlinker.github.io/static/css/main.7f150628.chunk.css"
+  },
+  {
+    "revision": "45fd5836b82a21efa67c",
+    "url": "/learnlinker.github.io/static/js/2.d8eb43be.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/learnlinker.github.io/static/js/2.d8eb43be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "136ff3353936c13c9cd8",
+    "url": "/learnlinker.github.io/static/js/main.85b429f3.chunk.js"
+  },
+  {
+    "revision": "776d072be9b780b8f449",
+    "url": "/learnlinker.github.io/static/js/runtime-main.1a505f07.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.65312d56b49aecc15bd34d33bce717ed.js"
+  "/learnlinker.github.io/precache-manifest.b0f40726c982e06ba136cba24fe80762.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/learnlinker.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
