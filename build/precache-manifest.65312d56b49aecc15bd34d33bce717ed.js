a386cf94a4fdc53ab42dd1ec27ce35a9e294fed3
saini11shubh <107746129+saini11shubh@users.noreply.github.com>
2023-10-08 18:31:28 +0530

    first commit

---
+++ build/precache-manifest.65312d56b49aecc15bd34d33bce717ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30af1c56e27d7f9fdf4b575db6d93df2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "136ff3353936c13c9cd8",
+    "url": "/static/css/main.7f150628.chunk.css"
+  },
+  {
+    "revision": "45fd5836b82a21efa67c",
+    "url": "/static/js/2.d8eb43be.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.d8eb43be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "136ff3353936c13c9cd8",
+    "url": "/static/js/main.85b429f3.chunk.js"
+  },
+  {
+    "revision": "97c002b8f8a25334b102",
+    "url": "/static/js/runtime-main.67d84520.js"
+  }
+]);
